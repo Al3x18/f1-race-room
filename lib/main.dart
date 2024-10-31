cde5c91267a03942176a66e05388703361aa08eb
Alex De Pasquale <alexdepasquale1997@gmail.com>
2024-10-31 16:10:34 +0100

    logic to load theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/route_manager.dart';
 import 'package:race_room/screens/root_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  final String themePrefs = await getThemePrefs();
   
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: _buildLightTheme(),
       darkTheme: _buildDarkTheme(),
-      themeMode: ThemeMode.light,
+      themeMode: getThemeMode(themePrefs),
       home: const RootView(),
     ),
   );
 }
+
+  Future<String> getThemePrefs() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('theme') ?? 'light';
+  }
+
+  ThemeMode getThemeMode(String themePref) {
+    switch (themePref) {
+      case 'Light':
+        return ThemeMode.light;
+      case 'Dark':
+        return ThemeMode.dark;
+      case 'System':
+        return ThemeMode.system;
+      default:
+        return ThemeMode.system;
+    }
+  }
 
 ThemeData _buildLightTheme() {
   return ThemeData(
