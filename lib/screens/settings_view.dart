c7d1ebf319dcac0a968ba5e81a459f8151da460c
Alex De Pasquale <alexdepasquale1997@gmail.com>
2024-10-31 17:43:00 +0100

    dev mail added

--- lib/screens/settings_view.dart
+++ lib/screens/settings_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:race_room/utils/not_share.dart';
 import 'package:race_room/utils/settings_controller.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -21,8 +22,9 @@ class _SettingsViewState extends State<SettingsView> {
   String version = "Unknown";
   String buildNumber = "Unknown";
 
-  String devMail = "smith@example.com";
-  String initialSubject = "Report dated: ${DateTime.now().toString()}";
+  final String devMail = REPORT_BUG_EMAIL;
+  final String devName = DEV_NAME;
+  final String initialSubject = "Report dated: ${DateTime.now().toString()}";
 
   final SettingsController settingsController = Get.find<SettingsController>();
 
@@ -66,94 +68,97 @@ class _SettingsViewState extends State<SettingsView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Developed by:", style: devByStyle),
-            Text("Alex De Pasquale", style: devByStyle),
-          ],
-        ),
-        ListTile(
-          leading: const Icon(Icons.info_outline),
-          title: const Text("App Version"),
-          trailing: Text(
-            version,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-          ),
-          subtitle: Text("Build: $buildNumber", style: const TextStyle(color: Colors.grey, fontSize: 11)),
-        ),
-        ListTile(
-          leading: const Icon(Icons.mail_outline),
-          title: const Text("Report an Issue"),
-          subtitle: const Text(
-            "or request new feature",
-            style: TextStyle(color: Colors.grey, fontSize: 11),
-          ),
-          trailing: const Icon(Icons.chevron_right),
-          onTap: () => _sendEmail(),
-        ),
-        ExpansionTile(
-          title: const Text("Theme Mode"),
-          leading: const Icon(Icons.display_settings_outlined),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 4),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Obx(() {
-                return Text(
-                  settingsController.currentTheme.value == ThemeMode.light
-                      ? "Light"
-                      : settingsController.currentTheme.value == ThemeMode.dark
-                          ? "Dark"
-                          : "System",
-                  style: const TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
-                );
-              }),
-              const SizedBox(width: 10),
-              const Icon(Icons.keyboard_arrow_down),
+              Text("Developed by:", style: devByStyle),
+              Text(devName, style: devByStyle),
             ],
           ),
-          children: [
-            Obx(() {
-              return ListTile(
-                leading: const Icon(Icons.light_mode_outlined),
-                title: const Text("Light"),
-                onTap: () {
-                  settingsController.setTheme("Light");
-                },
-                trailing: settingsController.currentTheme.value == ThemeMode.light
-                    ? const Icon(Icons.check)
-                    : null,
-              );
-            }),
-            Obx(() {
-              return ListTile(
-                leading: const Icon(Icons.sync),
-                title: const Text("System"),
-                onTap: () {
-                  settingsController.setTheme("System");
-                },
-                trailing: settingsController.currentTheme.value == ThemeMode.system
-                    ? const Icon(Icons.check)
-                    : null,
-              );
-            }),
-            Obx(() {
-              return ListTile(
-                leading: const Icon(Icons.dark_mode_outlined),
-                title: const Text("Dark"),
-                onTap: () {
-                  settingsController.setTheme("Dark");
-                },
-                trailing: settingsController.currentTheme.value == ThemeMode.dark
-                    ? const Icon(Icons.check)
-                    : null,
-              );
-            }),
-          ],
-        ),
-      ],
+          ListTile(
+            leading: const Icon(Icons.info_outline),
+            title: const Text("App Version"),
+            trailing: Text(
+              version,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+            ),
+            subtitle: Text("Build: $buildNumber", style: const TextStyle(color: Colors.grey, fontSize: 11)),
+          ),
+          ListTile(
+            leading: const Icon(Icons.mail_outline),
+            title: const Text("Report an Issue"),
+            subtitle: const Text(
+              "or request new feature",
+              style: TextStyle(color: Colors.grey, fontSize: 11),
+            ),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => _sendEmail(),
+          ),
+          ExpansionTile(
+            title: const Text("Theme Mode"),
+            leading: const Icon(Icons.display_settings_outlined),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Obx(() {
+                  return Text(
+                    settingsController.currentTheme.value == ThemeMode.light
+                        ? "Light"
+                        : settingsController.currentTheme.value == ThemeMode.dark
+                            ? "Dark"
+                            : "System",
+                    style: const TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
+                  );
+                }),
+                const SizedBox(width: 10),
+                const Icon(Icons.keyboard_arrow_down),
+              ],
+            ),
+            children: [
+              Obx(() {
+                return ListTile(
+                  leading: const Icon(Icons.light_mode_outlined),
+                  title: const Text("Light"),
+                  onTap: () {
+                    settingsController.setTheme("Light");
+                  },
+                  trailing: settingsController.currentTheme.value == ThemeMode.light
+                      ? const Icon(Icons.check)
+                      : null,
+                );
+              }),
+              Obx(() {
+                return ListTile(
+                  leading: const Icon(Icons.sync),
+                  title: const Text("System"),
+                  onTap: () {
+                    settingsController.setTheme("System");
+                  },
+                  trailing: settingsController.currentTheme.value == ThemeMode.system
+                      ? const Icon(Icons.check)
+                      : null,
+                );
+              }),
+              Obx(() {
+                return ListTile(
+                  leading: const Icon(Icons.dark_mode_outlined),
+                  title: const Text("Dark"),
+                  onTap: () {
+                    settingsController.setTheme("Dark");
+                  },
+                  trailing: settingsController.currentTheme.value == ThemeMode.dark
+                      ? const Icon(Icons.check)
+                      : null,
+                );
+              }),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
