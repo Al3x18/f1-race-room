ae3909476f959aeb08492b5edb1912bec5bfd036
Alex De Pasquale <alexdepasquale1997@gmail.com>
2024-11-02 14:49:53 +0100

    first commit

---
+++ lib/Utils/medal_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MedalColors {
+  static const Color gold = Color(0xFFFFD700);
+  static const Color silver = Color(0xFFC0C0C0);
+  static const Color bronze = Color(0xFFCD7F32);
+  static const Color lightBronze = Color(0xFFD9A066);
+  static const Color darkGold = Color(0xFFD4AF37); 
+}
