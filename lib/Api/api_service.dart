f81fdcee5fc38deceeb0aab2a7b0876cbf9ac6d1
Alex De Pasquale <alexdepasquale1997@gmail.com>
2024-11-02 13:55:22 +0100

    new api to fetch standings after round

--- lib/Api/api_service.dart
+++ lib/Api/api_service.dart
@@ -91,4 +91,48 @@ class ApiService {
       return null;
     }
   }
+
+    Future<MRDataDriverStandings?> fetchDriverStandingsToRound({required String seasonYear, required round}) async {
+    String apiUrl = 'https://api.jolpi.ca/ergast/f1/$seasonYear/$round/driverstandings/?format=json';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        // UTF-8 decoding is required for special characters like accents (ex. Pérez)
+        final Map<String, dynamic> jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
+
+        return MRDataDriverStandings.fromJson(jsonResponse['MRData']);
+      } else {
+        _logger.w('API Call error: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      _logger.e('API Call error: $e');
+      return null;
+    }
+  }
+
+  Future<MRDataConstructorStandings?> fetchConstructorStandingsToRound({required String seasonYear, required round}) async {
+    String apiUrl = 'https://api.jolpi.ca/ergast/f1/$seasonYear/$round/constructorstandings/?format=json';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        // UTF-8 decoding is required for special characters like accents (ex. Pérez)
+        final Map<String, dynamic> jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
+
+        return MRDataConstructorStandings.fromJson(jsonResponse['MRData']);
+      } else {
+        _logger.w('API Call error: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      _logger.e('API Call error: $e');
+      return null;
+    }
+  }
 }
+
+
