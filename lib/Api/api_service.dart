3f4d5d461d86b148f89287a60322c7d8614b13dc
Alex De Pasquale <alexdepasquale1997@gmail.com>
2024-11-22 00:54:03 +0100

    function to fetch weather data

--- lib/Api/api_service.dart
+++ lib/Api/api_service.dart
@@ -6,6 +6,8 @@ import 'package:race_room/model/driver_laps_model.dart';
 import 'package:race_room/model/driver_standings_model.dart';
 import 'package:race_room/model/race_results_model.dart';
 import 'package:race_room/model/race_schedule_model.dart';
+import 'package:race_room/model/weather_model.dart';
+import 'package:race_room/utils/not_share.dart';
 
 class ApiService {
   final Logger _logger = Logger();
@@ -154,5 +156,29 @@ class ApiService {
       _logger.e('API Call error: $e');
       return null;
     }
+  }
+
+  Future<WeatherData?> fetchTrackWeather({required String circuitLat, required String circuitLng}) async {
+    String apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=$circuitLat&lon=$circuitLng&appid=$OPEN_WEATHER_API_KEY&units=metric";
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+
+        return WeatherData.fromJson(jsonResponse);
+      } else {
+        _logger.w('API Call error: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      _logger.e('API Call error: $e');
+      return null;
+    }
+  }
+
+  String getWeatherIconUrl(String iconCode) {
+    return "https://openweathermap.org/img/wn/$iconCode@2x.png";
   }
 }
